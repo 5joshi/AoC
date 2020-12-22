a9a7cf783897e92c41d6b48c3f6f1511d997a3bd
5joshi <joshikaposhi@hotmail.com>
2020-12-22 13:22:33 +0100

    small day 22 optimisations

--- 2020/day22.py
+++ 2020/day22.py
@@ -20,47 +20,42 @@ def solve1(d):
     while p1 and p2:
         c1, c2 = p1.pop(0), p2.pop(0)
         if c1 > c2:
-            p1.append(c1)
-            p1.append(c2)
+            p1 += [c1, c2]
         else:
-            p2.append(c2)
-            p2.append(c1)
+            p2 += [c2, c1]
     for i, c in enumerate(reversed(p1+p2)):
         result += (i+1)*c
     return result
 
 
-def hash(p):
-    n = 0
+def hash(p, n=0):
     for x in p:
-        n = (n << 8) + x
+        n = (n << 6) + x
     return n
 
 
-def solve(d):
+def solve2(d):
     player1, player2 = lmap(lambda x: ints(x)[1:], d.split("\n\n"))
     result = 0
 
     def combat(p1, p2):
         prev_pos = set()
-        while p1 and p2 and (hash(p1), hash(p2)) not in prev_pos:
-            prev_pos.add((hash(p1), hash(p2)))
+        h1, h2 = (hash(p1), hash(p2))
+        while p1 and p2 and (h1, h2) not in prev_pos:
+            prev_pos.add((h1, h2))
             c1, c2 = p1.pop(0), p2.pop(0)
             if len(p1) < c1 or len(p2) < c2:
                 if c1 > c2:
-                    p1.append(c1)
-                    p1.append(c2)
+                    p1 += [c1, c2]
                 else:
-                    p2.append(c2)
-                    p2.append(c1)
+                    p2 += [c2, c1]
             else:
                 if combat(p1[:c1], p2[:c2])[0] == 1:
-                    p1.append(c1)
-                    p1.append(c2)
+                    p1 += [c1, c2]
                 else:
-                    p2.append(c2)
-                    p2.append(c1)
-        if (hash(p1), hash(p2)) in prev_pos or not p2:
+                    p2 += [c2, c1]
+            h1, h2 = (hash(p1), hash(p2))
+        if (h1, h2) in prev_pos or not p2:
             return (1, p1)
         else:
             return (2, p2)
@@ -86,6 +81,11 @@ Player 2:
 """
 s2 = """"""
 
-print("Example Solution:", solve(s))
+print("PART 1")
+print("Example Solution:", solve1(s))
 # print("Example 2 Solution:", solve(s2))
-print("Actual Solution:", solve(inp))
+print("Actual Solution:", solve1(inp))
+print("PART 2")
+print("Example Solution:", solve2(s))
+# print("Example 2 Solution:", solve(s2))
+print("Actual Solution:", solve2(inp))
