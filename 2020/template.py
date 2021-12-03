a68995e7b0cf07d1b4ebf11f93ba1b56ac3d05d8
JoshiDhima <s0190681@ad.ua.ac.be>
2021-12-03 08:47:11 +0100

    update templates

--- 2020/template.py
+++ 2020/template.py
@@ -11,7 +11,7 @@ from utils import *
 from functools import reduce
 from aocd import get_data, submit
 
-inp = get_data()
+inp = get_data(year=2020)
 
 
 def solve1(d):
@@ -32,10 +32,10 @@ s2 = """
 
 print("PART 1")
 print("Example Solution:", solve1(s))
-# print("Example 2 Solution:", solve(s2))
+# print("Example 2 Solution:", solve1(s2))
 print("Actual Solution:", solve1(inp))
 
 print("PART 2")
 print("Example Solution:", solve2(s))
-# print("Example 2 Solution:", solve(s2))
+# print("Example 2 Solution:", solve2(s2))
 print("Actual Solution:", solve2(inp))
