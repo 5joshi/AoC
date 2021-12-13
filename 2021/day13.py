9be5a98bbdf9745ffbb877d4ae0054df86940652
JoshiDhima <s0190681@ad.ua.ac.be>
2021-12-13 07:08:47 +0100

    day 13

---
+++ 2021/day13.py
@@ -0,0 +1,104 @@
+import collections as coll
+import datetime as dt
+import functools as ft
+import itertools as it
+import math
+from operator import itemgetter as ig
+import pprint as pp
+import re
+from copy import deepcopy
+from utils import *
+from functools import reduce
+from aocd import get_data, submit
+
+inp = get_data(year=2021, day=13)
+
+
+def solve1(d):
+    coords, folds = lmap(lambda x: x.splitlines(), d.split("\n\n"))
+    grid = set()
+    
+    for coord in coords:
+        grid.add(tuple(ints(coord)))
+    
+    for fold in folds[:1]:
+        axis, num = fold.split()[-1].split("=")
+        axis = int(axis == "y")
+        num = int(num)
+        new_grid = set()
+        
+        for coord in lmap(list, grid):
+            if coord[axis] > num:
+                coord[axis] = (num * 2) - coord[axis]
+            new_grid.add(tuple(coord))
+                
+        grid = new_grid
+
+    return len(grid)
+
+def solve2(d):
+    coords, folds = lmap(lambda x: x.splitlines(), d.split("\n\n"))
+    grid = set()
+    
+    def grid_str(grid):
+        result = "\n"
+        l = max([y for (_, y) in grid]) + 1
+        w = max([x for (x, _) in grid]) + 1
+        for y in range(l):
+            result += " ".join(["#" if (x, y) in grid else "." for x in range(w)]) + "\n"
+        return result
+    
+    for coord in coords:
+        grid.add(tuple(ints(coord)))
+    
+    for fold in folds:
+        axis, num = fold.split()[-1].split("=")
+        axis = int(axis == "y")
+        num = int(num)
+        new_grid = set()
+        
+        for coord in lmap(list, grid):
+            if coord[axis] > num:
+                coord[axis] = (num * 2) - coord[axis]
+            new_grid.add(tuple(coord))
+                
+        grid = new_grid
+
+    result = grid_str(grid)
+    return result
+
+
+s = """6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5
+"""
+s2 = """
+"""
+
+print("PART 1")
+print("Example Solution:", solve1(s))
+# print("Example 2 Solution:", solve1(s2))
+print("Actual Solution:", solve1(inp))
+
+print("PART 2")
+print("Example Solution:", solve2(s))
+# print("Example 2 Solution:", solve2(s2))
+print("Actual Solution:", solve2(inp))
