166b366a2b99c70575edd3fba8dd20acc132d806
JoshiDhima <joshikaposhi@hotmail.com>
2023-12-06 06:24:06 +0100

    day 6 cleanup

--- 2023/day06.py
+++ 2023/day06.py
@@ -28,11 +28,9 @@ s2 = """
 print("PART 1")
 print("Example Solution:", solve1(s))
 # print("Example 2 Solution:", solve1(s2))
-if inp != "\n":
-    print("Actual Solution:", solve1(inp))
+print("Actual Solution:", solve1(inp))
 
 print("PART 2")
 print("Example Solution:", solve2(s))
 # print("Example 2 Solution:", solve2(s2))
-if inp != "\n":
-    print("Actual Solution:", solve2(inp))
+print("Actual Solution:", solve2(inp))
