03dc92895132e294112626b9313237fd2c1e277e
JoshiDhima <joshikaposhi@hotmail.com>
2023-12-12 09:58:11 +0100

    day 12 rewrite

--- 2023/day12.py
+++ 2023/day12.py
@@ -3,116 +3,23 @@ from utils import *
 YEAR, DAY = ints(__file__)
 inp = get_data(year=YEAR, day=DAY)
 
-def solve1(d):
-    lines = d.splitlines()
-    result = 0
-    
-    for line in lines:
-        s, nums = line.split()
-        qs = line.count('?')
-        possibilities = it.product('#.', repeat=qs)
-        for possib in possibilities:
-            s2 = ""
-            idx = 0
-            for c in s:
-                if c != '?':
-                    s2 += c
-                else:
-                    s2 += possib[idx]
-                    idx += 1
-                    
-            lens = [len(list(g)) for k, g in it.groupby(s2) if k == '#']
-            # print(s2, lens)
-            if lens == ints(nums):
-                result += 1
-
-    
-    return result
-
-
-
-    # print(s, nums)
-
 @lru_cache
-def possibs(s, nums):
-    # print(s, nums)
-    if nums[0] == 0:
-        if s[0] == '#':
-            return 0 
-        elif s[0] == '?':
-            return possibs(s[1:], nums[1:])
-        nums = nums[1:]
-    if nums == ():
-        # print(s, nums)
+def count(s, nums):
+    s = s.strip('.') + '.'
+    if s == '.': return sum(nums) == 0
+    if sum(nums) == 0: return '#' not in s
+    if sum(nums) > len(s) - 1: return 0
 
-        if s.count('#') == 0:
-            return 1
-        else:
-            return 0
-    elif s.count('?') + s.count('#') < sum(nums):
-        return 0
-    elif s.count('?') + s.count('#') == sum(nums):
-        tmp = tmap(len, re.split(r'\.+', s.strip('.')))
-        print(s, nums, tmp, tmp == nums)
-        if tmp == nums:
-            return 1
-        else:
-            return 0
-    else:
-        if s[0] == '?':
-            result = possibs(s[1:], nums) + possibs(s[1:], (nums[0] - 1,) + nums[1:])
-            # return possibs(s[1:], nums) + possibs(s[1:], (nums[0] - 1,) + nums[1:])          
-        elif s[0] == '#':
-            result = possibs(s[1:], (nums[0] - 1,) + nums[1:])  
-            # return possibs(s[1:], (nums[0] - 1,) + nums[1:])       
-        else:
-            result = possibs(s[1:], nums)
-            # return possibs(s[1:], nums)   
-        if result == 1:
-            
-            print(s, nums)
-        return result
-    
-@lru_cache(maxsize=None)
-def possibs(s, nums, block=False):
-    left = sum(nums)
+    if s[0] == '#': return count(s[nums[0]+1:], nums[1:]) if re.fullmatch('[?#]+[\.?]', s[:nums[0]+1]) else 0
+    return count(s[1:], nums) + count('#' + s[1:], nums)
 
-    
-    
-    if s == '':
-        return left == 0
-    if left == 0:
-        return not '#' in s
-    
-    if s[0] == '#':
-        if nums[0] == 0 and block:
-            return 0
-        return possibs(s[1:], (nums[0] - 1,) + nums[1:], True)
-    
-    if s[0] == '.':
-        if nums[0] > 0 and block:
-            return 0
-        return possibs(s[1:], nums[1:] if nums[0] == 0 else nums, False)
-    
-    if nums[0] == 0 and block:
-        return possibs(s[1:], nums[1:], False)
-    if block:
-        return possibs(s[1:], (nums[0] - 1,) + nums[1:], True)
-    return possibs(s[1:], (nums[0] - 1,) + nums[1:], True) + possibs(s[1:], nums, False)
-        
 
+def solve1(d):
+    return sum(count(s, tuple(ints(nums))) for s, nums in map(lambda line: line.split(), d.splitlines()))
 
 def solve2(d):
-    lines = d.splitlines()
-    result = 0
-    
-    for line in lines:
-        s, nums = line.split()
-        s = '?'.join([s] * 5)
-        nums = ints(nums) * 5
-        result += possibs(s, tuple(nums))
+    return sum(count('?'.join([s] * 5), tuple(ints(nums)*5)) for s, nums in map(lambda line: line.split(), d.splitlines()))
 
-    return result
 
 
 s = """
