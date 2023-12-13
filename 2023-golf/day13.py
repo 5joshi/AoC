b0c826780f9b8aa4a944c2980c704392453dffa6
JoshiDhima <joshikaposhi@hotmail.com>
2023-12-13 09:24:07 +0100

    improve day 13 golf

--- 2023-golf/day13.py
+++ 2023-golf/day13.py
@@ -4,11 +4,17 @@ d = get_data(year=2023, day=13).split('\n\n')
 
 # PART 1
 
-e=lambda r:[i for i in range(len(r))if all(x==y for x,y in zip(r[i:],r[:i][::-1]))][-1]
-print(sum(100*e(r:=[*map(list,g.split('\n'))])or e([*zip(*r)])for g in d))
+# e=lambda r:[i for i in range(len(r))if all(x==y for x,y in zip(r[i:],r[:i][::-1]))][-1]
+# print(sum(100*e(r:=[*map(list,g.split('\n'))])or e([*zip(*r)])for g in d))
+
+e=lambda r:sum(i*all(x==y for x,y in zip(r[i:],r[:i][::-1]))for i in range(len(r)))
+print(sum(100*e(r:=g.split('\n'))or e([*zip(*r)])for g in d))
 
 
 # PART 2
 
-e=lambda r:([i for i in range(len(r))if sum(sum(a!=b for a,b in zip(x,y)) for x,y in zip(r[i:],r[:i][::-1]))==1]or[0])[-1]
-print(sum(100*e(r:=[*map(list,g.split('\n'))])or e([*zip(*r)])for g in d))
+# e=lambda r:([i for i in range(len(r))if sum(sum(a!=bfor a,b in zip(x,y))for x,y in zip(r[i:],r[:i][::-1]))==1]or[0])[-1]
+# print(sum(100*e(r:=[*map(list,g.split('\n'))])or e([*zip(*r)])for g in d))
+
+e=lambda r:sum(i*(sum(sum(a!=b for a,b in zip(x,y))for x,y in zip(r[i:],r[:i][::-1]))==1)for i in range(len(r)))
+print(sum(100*e(r:=g.split('\n'))or e([*zip(*r)])for g in d))
