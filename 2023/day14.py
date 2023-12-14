ded71f6c453d5a95cb98fc2017ad50e12ba92524
JoshiDhima <joshikaposhi@hotmail.com>
2023-12-14 06:56:22 +0100

    day 14 initial commit

---
+++ 2023/day14.py
@@ -0,0 +1,124 @@
+from utils import *
+from copy import deepcopy
+
+YEAR, DAY = ints(__file__)
+inp = get_data(year=YEAR, day=DAY)
+
+def solve1(d):
+    grid = s_to_grid(d)
+    result = 0
+    
+    for col in grid.cols():
+        row = len(col)
+        for idx, elem in enumerate(col):
+            if elem == 'O':
+                result += row
+                row -= 1
+            elif elem == '#':
+                row = len(col) - idx - 1
+    
+    # def roll(grid, direction):
+    #     new_grid = grid
+    #     for c in grid.findall('O'):
+    #         if grid[c] in '.#': continue
+    #         new = tadd(c, direction)
+    #         if grid[new] == '.':
+    #             new_grid[new] = 'O'
+    #             new_grid[c] == '.'
+    #         else:
+    #             new_grid[c] = 'O'
+    #     return new_grid
+    
+    
+    # while grid != (new_grid := roll(grid, CHAR_TO_DELTA['U'])):
+    #     grid = new_grid
+    #     print(new_grid, "\n")
+
+        
+        
+    return result
+
+def solve2(d):
+    grid = Grid(lmap(list, d.splitlines()))
+    length = grid.nrows
+    print(grid, grid.ncols, grid.nrows)
+    cycle = []
+    seen = set()
+    d = it.cycle('NWSE')
+    
+    def roll(grid, direction):
+        new_grid = grid
+        if direction == 'N':
+            order = grid.findall('O')
+        if direction == 'S':
+            order = reversed(grid.findall('O'))
+        if direction == 'W':
+            order = sorted(grid.findall('O'), key=snd)
+        if direction == 'E':
+            order = sorted(grid.findall('O'), key=snd, reverse=True)
+        for c in order:
+            old = c
+            new = tadd(c, CHAR_TO_DELTA[direction])
+            while new in grid and grid[new] == '.':
+                new_grid[new] = 'O'
+                new_grid[old] = '.'
+                old = new
+                new = tadd(new, CHAR_TO_DELTA[direction])
+        return new_grid
+               
+    while str(grid) not in seen:
+    # for _ in range(12):
+        seen.add(str(grid))
+        cycle.append(str(grid))
+        grid = roll(grid, (curr :=next(d)))  
+        # if curr == 'E':
+        #     print(grid, '\n')       
+    
+    frm = cycle.index(str(grid))
+    length = len(cycle) - frm
+    end = cycle[frm + (4000000000 - frm) % length]
+    end = Grid(lmap(list, end.splitlines())) 
+    result = 0
+    for c in end.findall('O'):
+        result += grid.nrows - c[0]
+    return result
+    # print(grid, "\n")
+    # print(roll(grid, CHAR_TO_DELTA['U']), "\n")
+    # return result
+
+
+s = """
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+
+""".strip()
+s2 = """
+
+""".strip()
+
+if __name__ == '__main__':
+    one, two, e1, e2, ex1, ex2, r1, r2 = get_solution_booleans(sys.argv)
+            
+    if e1 or ex1 or r1: print("PART 1")
+    if e1 and s != "": print("Example Solution:", solve1(s))
+    if ex1: print("Example 2 Solution:", solve1(s2))
+    if r1: print("Actual Solution:", sol1 := solve1(inp))
+
+    if e2 or ex2 or r2: print("PART 2")
+    if e2 and s != "": print("Example Solution:", solve2(s))
+    if ex2: print("Example 2 Solution:", solve2(s2))
+    if r2: print("Actual Solution:", sol2 := solve2(inp))
+    
+    if (one and r1) or (two and r2):
+        go = input('Submit? [y/N] ')
+        if go == 'y':
+            if one and r1: submit(sol1, part=1, year=YEAR, day=DAY)
+            if two and r2: submit(sol2, part=2, year=YEAR, day=DAY)
