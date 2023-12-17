657cf2cdb13bdae3805a114976eeaf3d99973e72
JoshiDhima <joshikaposhi@hotmail.com>
2023-12-17 09:40:59 +0100

    day 25

---
+++ 2016/day25.py
@@ -0,0 +1,28 @@
+from utils import *
+
+YEAR, DAY = ints(__file__)
+inp = get_data(year=YEAR, day=DAY)
+
+def solve1(d):
+    const = product(ints(d)[:2])
+    return next(filter(lambda i: re.fullmatch('(01)*', bin(i + const)[-1:1:-1]), it.count(1)))
+
+s = """
+
+""".strip()
+s2 = """
+
+""".strip()
+
+if __name__ == '__main__':
+    one, two, e1, e2, ex1, ex2, r1, r2 = get_solution_booleans(sys.argv)
+            
+    if e1 or ex1 or r1: print("PART 1")
+    if e1 and s != "": print("Example Solution:", solve1(s))
+    if ex1: print("Example 2 Solution:", solve1(s2))
+    if r1: print("Actual Solution:", sol1 := solve1(inp))
+    
+    if (one and r1) or (two and r2):
+        go = input('Submit? [y/N] ')
+        if go == 'y':
+            if one and r1: submit(sol1, part=1, year=YEAR, day=DAY)
