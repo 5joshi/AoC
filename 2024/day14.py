8e835037e250b77658fa1bf3a30485a05aa46050
JoshiDhima <joshikaposhi@hotmail.com>
2024-12-14 11:48:43 +0100

    cleanup day 14

--- 2024/day14.py
+++ 2024/day14.py
@@ -3,45 +3,23 @@ from utils import *
 YEAR, DAY = ints(__file__)
 inp = get_data(year=YEAR, day=DAY)
 
-def solve1(d):
+def solve1(d, w=101, h=103):
     robots = lmap(ints, d.splitlines())
-    points = defaultdict(set) 
-    for px, py, vx, vy in robots:
-        points[(px, py)].add((vx, vy))
-
-    maxx, maxy = max(map(fst, points.keys())), max(map(snd, points.keys()))
-
-    for _ in range(100):
-        new_points = defaultdict(set)
-        for p, s in points.items():
-            for v in s:
-                nx, ny = tadd(p, v)
-                new_points[(nx % (maxx + 1), ny % (maxy + 1))].add(v)
-        points = new_points
+    points = [((px + (vx * 100)) % w, (py + (vy * 100)) % h) for px, py, vx, vy in robots]
     
     quadrants = [0] * 4
-    for x, y in points.keys():
-        if x == (maxx // 2) or y == (maxy // 2): continue
-        quadrants[(x > (maxx // 2)) + 2 * (y > (maxy // 2))] += len(points[(x, y)])
-        
+    for x, y in points:
+        if x == (w // 2) or y == (h // 2): continue
+        quadrants[(x > (w // 2)) + 2 * (y > (h // 2))] += 1
+    
     return product(quadrants)
 
-def solve2(d):
+def solve2(d, w=101, h=103):
     robots = lmap(ints, d.splitlines())
-    points = defaultdict(set) 
-    for px, py, vx, vy in robots:
-        points[(px, py)].add((vx, vy))
-
-    maxx, maxy = max(map(fst, points.keys())), max(map(snd, points.keys()))
-
-    for time in range(10000):
-        new_points = defaultdict(set)
-        if all(len(s) == 1 for s in points.values()): return time
-        for p, s in points.items():
-            for v in s:
-                nx, ny = tadd(p, v)
-                new_points[(nx % (maxx + 1), ny % (maxy + 1))].add(v)
-        points = new_points
+    for i in range(10000):
+        points = {((px + (vx * i)) % w, (py + (vy * i)) % h) for px, py, vx, vy in robots}
+        if len(points) == len(robots): 
+            return i
 
 s = """
 p=0,4 v=3,-3
@@ -67,13 +45,13 @@ if __name__ == '__main__':
     one, two, e1, e2, ex1, ex2, r1, r2 = get_solution_booleans(sys.argv)
             
     if e1 or ex1 or r1: print("PART 1")
-    if e1 and s != "": print("Example Solution:", solve1(s))
-    if ex1: print("Example 2 Solution:", solve1(s2))
+    if e1 and s != "": print("Example Solution:", solve1(s, w=11, h=7))
+    if ex1: print("Example 2 Solution:", solve1(s2, w=11, h=7))
     if r1: print("Actual Solution:", sol1 := solve1(inp))
 
     if e2 or ex2 or r2: print("PART 2")
-    if e2 and s != "": print("Example Solution:", solve2(s))
-    if ex2: print("Example 2 Solution:", solve2(s2))
+    if e2 and s != "": print("Example Solution:", solve2(s, w=11, h=7))
+    if ex2: print("Example 2 Solution:", solve2(s2, w=11, h=7))
     if r2: print("Actual Solution:", sol2 := solve2(inp))
     
     if (one and r1) or (two and r2):
