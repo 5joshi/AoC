8e96c88f1a7379df3c0cbf3daa9ce973db154bc0
JoshiDhima <joshikaposhi@hotmail.com>
2024-12-16 08:54:44 +0100

    day 10

---
+++ 2018/day10.py
@@ -0,0 +1,79 @@
+from utils import *
+
+YEAR, DAY = ints(__file__)
+inp = get_data(year=YEAR, day=DAY)
+
+def solve1(d):
+    light = lmap(ints, d.splitlines())
+    smallest, grid = 100, None
+    for i in range(20000):
+        points = {((px + (vx * i)), (py + (vy * i))) for px, py, vx, vy in light}
+        if (d := max_minus_min(lmap(fst, points))) < smallest:
+            smallest, grid = d, points_to_grid(points, flip=True)
+    return "\n" + str(grid)
+
+def solve2(d):
+    light = lmap(ints, d.splitlines())
+    smallest, idx = 100, 0
+    for i in range(20000):
+        points = {((px + (vx * i)), (py + (vy * i))) for px, py, vx, vy in light}
+        if (d := max_minus_min(lmap(fst, points))) < smallest:
+            smallest, idx = d, i
+    return idx
+
+
+s = """
+position=< 9,  1> velocity=< 0,  2>
+position=< 7,  0> velocity=<-1,  0>
+position=< 3, -2> velocity=<-1,  1>
+position=< 6, 10> velocity=<-2, -1>
+position=< 2, -4> velocity=< 2,  2>
+position=<-6, 10> velocity=< 2, -2>
+position=< 1,  8> velocity=< 1, -1>
+position=< 1,  7> velocity=< 1,  0>
+position=<-3, 11> velocity=< 1, -2>
+position=< 7,  6> velocity=<-1, -1>
+position=<-2,  3> velocity=< 1,  0>
+position=<-4,  3> velocity=< 2,  0>
+position=<10, -3> velocity=<-1,  1>
+position=< 5, 11> velocity=< 1, -2>
+position=< 4,  7> velocity=< 0, -1>
+position=< 8, -2> velocity=< 0,  1>
+position=<15,  0> velocity=<-2,  0>
+position=< 1,  6> velocity=< 1,  0>
+position=< 8,  9> velocity=< 0, -1>
+position=< 3,  3> velocity=<-1,  1>
+position=< 0,  5> velocity=< 0, -1>
+position=<-2,  2> velocity=< 2,  0>
+position=< 5, -2> velocity=< 1,  2>
+position=< 1,  4> velocity=< 2,  1>
+position=<-2,  7> velocity=< 2, -2>
+position=< 3,  6> velocity=<-1, -1>
+position=< 5,  0> velocity=< 1,  0>
+position=<-6,  0> velocity=< 2,  0>
+position=< 5,  9> velocity=< 1, -2>
+position=<14,  7> velocity=<-2,  0>
+position=<-3,  6> velocity=< 2, -1>
+""".strip()
+s2 = """
+
+""".strip()
+
+if __name__ == '__main__':
+    one, two, e1, e2, ex1, ex2, r1, r2 = get_solution_booleans(sys.argv)
+            
+    if e1 or ex1 or r1: print("PART 1")
+    if e1 and s != "": print("Example Solution:", solve1(s))
+    if ex1: print("Example 2 Solution:", solve1(s2))
+    if r1: print("Actual Solution:", sol1 := solve1(inp))
+
+    if e2 or ex2 or r2: print("PART 2")
+    if e2 and s != "": print("Example Solution:", solve2(s))
+    if ex2: print("Example 2 Solution:", solve2(s2))
+    if r2: print("Actual Solution:", sol2 := solve2(inp))
+    
+    if (one and r1) or (two and r2):
+        go = input('Submit? [y/N] ')
+        if go == 'y':
+            if one and r1: submit(sol1, part=1, year=YEAR, day=DAY)
+            if two and r2: submit(sol2, part=2, year=YEAR, day=DAY)
