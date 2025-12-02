65857d06841cf22735a6f9c7e548a33986e01f10
JoshiDhima <joshikaposhi@hotmail.com>
2025-12-02 11:39:05 +0100

    day 2

---
+++ 2025/day02.py
@@ -0,0 +1,55 @@
+from utils import *
+
+YEAR, DAY = ints(__file__)
+inp = get_data(year=YEAR, day=DAY)
+
+def solve1(d):
+    ranges = lmap(positive_ints, d.split(','))
+    result = 0
+    
+    for frm, to in ranges:
+        for i in range(frm, to+1):
+            tmp = str(i)
+            result += i * (tmp[:len(tmp) // 2] * 2 == tmp)
+    
+    return result
+
+def solve2(d):
+    ranges = lmap(positive_ints, d.split(','))
+    result = 0
+    
+    for frm, to in ranges:
+        for i in range(frm, to+1):
+            tmp = str(i)
+            result += i * any((tmp[:j] * (len(tmp) // j)) == tmp for j in range(1, len(tmp) // 2 + 1))
+    
+    return result
+                
+
+s = """
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
+""".strip()
+s2 = """
+
+""".strip()
+
+if __name__ == '__main__':
+    one, two, e1, e2, ex1, ex2, r1, r2 = get_solution_booleans(sys.argv)
+            
+    if e1 or ex1 or r1: print("PART 1")
+    if e1 and s != "": print("Example Solution:", solve1(s))
+    if ex1: print("Example 2 Solution:", solve1(s2))
+    if r1: print("Actual Solution:", sol1 := solve1(inp))
+
+    if e2 or ex2 or r2: print("PART 2")
+    if e2 and s != "": print("Example Solution:", solve2(s))
+    if ex2: print("Example 2 Solution:", solve2(s2))
+    if r2: print("Actual Solution:", sol2 := solve2(inp))
+    
+    if (one and r1) or (two and r2):
+        go = input('Submit? [y/N] ')
+        if go == 'y':
+            if one and r1: submit(sol1, part=1, year=YEAR, day=DAY)
+            if two and r2: submit(sol2, part=2, year=YEAR, day=DAY)
